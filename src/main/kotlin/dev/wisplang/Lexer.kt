1d62af3af340c9cef8a6a7753db6d761e386974b
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2022-07-01 16:02:27 -0500

    Set up lexer for parsing the tokens

--- src/main/kotlin/dev/wisplang/Lexer.kt
+++ src/main/kotlin/dev/wisplang/Lexer.kt
@@ -1,14 +1,39 @@
 package dev.wisplang
 
+import dev.wisplang.ast.DefinedFunction
+import dev.wisplang.ast.DefinedType
+import dev.wisplang.ast.DefinedVariable
+
 object Lexer {
     fun lex(tokens: List<Tokenizer.MatureToken>) {
         var i = 0
-        while (i < tokens.size) {
+        do {
             val token = tokens[i]
             when {
 
             }
-            i++
-        }
+        } while (++i < tokens.size)
+    }
+
+    fun parseFunction(idx: Int, tokens: List<Tokenizer.MatureToken>): Triple<DefinedFunction?, Int, String?> {
+        var i = idx
+        do {
+
+        } while (++i < tokens.size)
+        return Triple(null, i, null)
+    }
+    fun parseVariable(idx: Int, tokens: List<Tokenizer.MatureToken>): Triple<DefinedVariable?, Int, String?> {
+        var i = idx
+        do {
+
+        } while (++i < tokens.size)
+        return Triple(null, i, null)
+    }
+    fun parseType(idx: Int, tokens: List<Tokenizer.MatureToken>): Triple<DefinedType? ,Int, String?> {
+        var i = idx
+        do {
+
+        } while (++i < tokens.size)
+        return Triple(null, i, null)
     }
 }
