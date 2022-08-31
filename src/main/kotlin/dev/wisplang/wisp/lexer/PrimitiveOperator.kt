b9d0fca88707f331036c4051f41b5f445c714190
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-08-31 12:42:10 +0200

    remove unused class

--- src/main/kotlin/dev/wisplang/wisp/lexer/PrimitiveOperator.kt
+++
@@ -1,5 +0,0 @@
-package dev.wisplang.wisp.lexer
-
-data class PrimitiveOperator(val value: Operator, val operator: PrimitiveOperator? = null) {
-    constructor(value: String, operator: PrimitiveOperator? = null) : this(Operator.of(value), operator)
-}
