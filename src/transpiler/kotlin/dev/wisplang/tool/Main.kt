728d2677075e5471e2bf6023a20102fdeffafaf7
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-09-25 20:50:22 +0200

    package rename: transpiler -> tool

---
+++ src/transpiler/kotlin/dev/wisplang/tool/Main.kt
@@ -0,0 +1,23 @@
+package dev.wisplang.tool
+
+import dev.wisplang.wisp.lexer.Lexer
+import dev.wisplang.wisp.tokenizer.Tokenizer
+import java.io.File
+
+fun main(args: Array<String>) {
+    val tokenList = Tokenizer.tokenize( File( args[1] ).inputStream().readBytes().toString(Charsets.UTF_8) )
+    val matureTokens = Tokenizer.matureTokens(tokenList)
+    val root = Lexer().lex( matureTokens, args[1] )
+
+    when ( args[0] ) {
+        "javailer" -> {
+            val path = File( "./src/java/" )
+            path.mkdirs()
+
+            Transpiler( root, path ).java()
+        }
+        "astdump" -> {
+            AstPrinter( root, File(".") ).save()
+        }
+    }
+}
